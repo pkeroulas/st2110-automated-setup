6dd9848392a15318c53b20b29de0ea6228189e51
PK Config Laptop <patrick.keroulas@radio-canada.ca>
2023-05-09 09:19:41 -0400

    ansible/switch: overwrite port description in yml from NB

--- ansible/roles/get_netbox_data/library/get_netbox_data.py
+++ ansible/roles/get_netbox_data/library/get_netbox_data.py
@@ -21,9 +21,15 @@ file_handler.setFormatter(formatter)
 MODULE_LOGGER.addHandler(file_handler)
 MODULE_LOGGER.info('Start get_netbox_data module execution')
 
+yaml.preserve_quotes = True
+
+class IndentDumper(yaml.Dumper):
+  def increase_indent(self, flow=False, indentless=False):
+    return super(IndentDumper, self).increase_indent(flow, False)
+
 def write_yaml_file(filename, yaml_dict, sort=True):
   with open(filename, 'w') as f:
-    yaml.dump(yaml_dict, f,default_flow_style=False,indent=2,sort_keys=sort)
+    yaml.dump(yaml_dict, f, default_flow_style=False, indent=2, sort_keys=sort)
 
 def open_yaml_file(filename):
   with open(filename, 'r') as stream:
@@ -41,7 +47,6 @@ def hash_yaml_file(filename):
       hash_md5.update(data)
   except Exception as e:
     print('no-file {}',e)
-
   return hash_md5.hexdigest()
 
 def main():
@@ -49,39 +54,42 @@ def main():
     argument_spec=dict(
       nb_host=dict(type='str', required=True),
       token=dict(type='str', required=True),
-      config_file=dict(type='str', required=True),
+      config_dir=dict(type='str', required=True),
     ),
     supports_check_mode=True,
   )
   nb_host = module.params['nb_host']
   token = module.params['token']
-  config_file  = module.params['config_file']
-
+  config_dir  = module.params['config_dir']
   nb = pynetbox.api(nb_host,token)
   nb.http_session.verify = False
 
-  structured_config = open_yaml_file(config_file)
-  hash_init = hash_yaml_file(config_file)
-
-  devices=nb.dcim.devices.filter(role='standalone-media-switch')
-  fmt = "{:<20}{:<20}{:<30}{:<60}"
-  header = ("Name", "Device Role", "Description", "Config Context")
-  MODULE_LOGGER.info(fmt.format(*header))
+  devices = nb.dcim.devices.filter(role='standalone-media-switch')
+  has_changed = False
   for dev in devices:
     MODULE_LOGGER.info(f"{ dev.name }")
-    ifaces = list(nb.dcim.interfaces.filter(device=dev.name))
-    for iface in ifaces:
-      if iface.name in structured_config['ethernet_interfaces']:
-        MODULE_LOGGER.info(f"{ iface.name } FOUND ")
+    config_file = f"{ config_dir }/{ dev.name }.yml"
+    structured_config = open_yaml_file(config_file)
+    if structured_config == None:
+        continue
+    hash_init = hash_yaml_file(config_file)
+    MODULE_LOGGER.info(f"INIT {structured_config}")
+
+    nb_ifaces = list(nb.dcim.interfaces.filter(device=dev.name))
+    for nb_iface in nb_ifaces:
+      if nb_iface.name in structured_config['ethernet_interfaces'] and nb_iface.description != '':
+        MODULE_LOGGER.info(f"{ nb_iface.name } ({nb_iface.description}) FOUND ")
+        structured_config['ethernet_interfaces'][nb_iface.name]['description'] = nb_iface.description
     else:
-        MODULE_LOGGER.info(f"{ iface.name } NOT FOUND ")
+        MODULE_LOGGER.info(f"{ nb_iface.name } NOT FOUND ")
 
-  write_yaml_file(config_file, structured_config, False)
-  hash_end = hash_yaml_file(config_file)
-  #MODULE_LOGGER.info(f"HASH { hash_init } { hash_end }")
+    write_yaml_file(config_file, structured_config, False)
+    hash_end = hash_yaml_file(config_file)
+    MODULE_LOGGER.info(f"END {structured_config}")
+    MODULE_LOGGER.info(f"{config_file}: HASH { hash_init } -> { hash_end }")
+    has_changed = has_changed if hash_init == hash_end else True
 
-  changed = False if hash_init == hash_end else True
-  module.exit_json(changed=changed, msg=f"Received from NetBox, url= {module.params['nb_host']}")
+  module.exit_json(changed=has_changed, msg=f"Received from NetBox, url= {module.params['nb_host']}")
 
 if __name__ == "__main__":
   main()
