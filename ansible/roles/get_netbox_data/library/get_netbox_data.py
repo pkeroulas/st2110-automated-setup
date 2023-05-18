b08a9f9bccb03bf777dc8da38aca723b6fcd9fd5
Patrick Keroulas <patrick.keroulas@radio-canada.ca>
2023-05-18 17:38:43 -0400

    ansible: refactor get_netbox_data to use inventory_hostname in filenames

--- ansible/roles/get_netbox_data/library/get_netbox_data.py
+++ ansible/roles/get_netbox_data/library/get_netbox_data.py
@@ -78,68 +78,38 @@ STRUCTURE_PORT_TEMPLATE = {
   }
 }
 
-def process_switches(nb, config_dir, devices):
-  hash_init = hash_end = has_changed = False
-  for dev in devices:
-    MODULE_LOGGER.info(f"{ dev.name }")
-    config_file = f"{ config_dir }/{ dev.name.replace(' ', '_') }.yml"
-    structured_config = open_yaml_file(config_file)
-    if structured_config == None:
-        continue
-    hash_init = hash_yaml_file(config_file)
-    #MODULE_LOGGER.info(f"INIT {structured_config}")
+def process_switch(nb, struct_config, dev):
+  nb_ifaces = list(nb.dcim.interfaces.filter(device=dev.name))
+  for nb_iface in nb_ifaces:
+    if nb_iface.name == 'Ethernet48' or nb_iface.name == 'Management1': # Never touch the management link,
+      continue
 
-    nb_ifaces = list(nb.dcim.interfaces.filter(device=dev.name))
-    for nb_iface in nb_ifaces:
-      if nb_iface.name == 'Ethernet48' or nb_iface.name == 'Management1': # Never touch the management link,
-        continue
+    # add missing
+    if not nb_iface.name in struct_config['ethernet_interfaces']:
+      struct_config['ethernet_interfaces'][nb_iface.name] = copy.deepcopy(STRUCTURE_PORT_TEMPLATE)
 
-      # add missing
-      if not nb_iface.name in structured_config['ethernet_interfaces']:
-        structured_config['ethernet_interfaces'][nb_iface.name] = copy.deepcopy(STRUCTURE_PORT_TEMPLATE)
+    structured_iface = struct_config['ethernet_interfaces'][nb_iface.name]
+    structured_iface['description'] = nb_iface.description if nb_iface.description != '' else '_'
+    MODULE_LOGGER.info(f"{ nb_iface.name } ({ nb_iface.description })")
 
-      structured_iface = structured_config['ethernet_interfaces'][nb_iface.name]
-      structured_iface['description'] = nb_iface.description if nb_iface.description != '' else '_'
-      MODULE_LOGGER.info(f"{ nb_iface.name } ({ nb_iface.description })")
+    #VLANS
+    if nb_iface.mode == None: # delete iface
+        if hasattr(structured_iface, "mode"): del structured_iface['mode']
+        if hasattr(structured_iface, "vlans"): del structured_iface['vlans']
+    elif nb_iface.mode.value == 'access': # add access
+      structured_iface['mode'] = nb_iface.mode.value
+      structured_iface['vlans'] = nb_iface.untagged_vlan.vid
+    elif nb_iface.mode.value == 'tagged': # add trunk
+      structured_iface['mode'] = 'trunk'
+      structured_vids = ''
+      for vlan in nb_iface.tagged_vlans:
+        structured_vids += f"{ vlan.vid },"
+      structured_iface['vlans'] = structured_vids[:-1] #remove last ','
+      if nb_iface.untagged_vlan != None:
+        MODULE_LOGGER.info(f"What TODO with { nb_iface.untagged_vlan }")
 
-      #VLANS
-      if nb_iface.mode == None: # delete iface
-          if hasattr(structured_iface, "mode"): del structured_iface['mode']
-          if hasattr(structured_iface, "vlans"): del structured_iface['vlans']
-      elif nb_iface.mode.value == 'access': # add access
-        structured_iface['mode'] = nb_iface.mode.value
-        structured_iface['vlans'] = nb_iface.untagged_vlan.vid
-      elif nb_iface.mode.value == 'tagged': # add trunk
-        structured_iface['mode'] = 'trunk'
-        structured_vids = ''
-        for vlan in nb_iface.tagged_vlans:
-          structured_vids += f"{ vlan.vid },"
-        structured_iface['vlans'] = structured_vids[:-1] #remove last ','
-        if nb_iface.untagged_vlan != None:
-          MODULE_LOGGER.info(f"What TODO with { nb_iface.untagged_vlan }")
-
-    write_yaml_file(config_file, structured_config, False)
-    hash_end = hash_yaml_file(config_file)
-    #MODULE_LOGGER.info(f"END {structured_config}")
-    MODULE_LOGGER.info(f"{config_file}: HASH { hash_init } -> { hash_end }")
-
-  return has_changed if hash_init == hash_end else True
-
-def process_gateways(nb, config_dir, devices):
-  hash_init = hash_end = has_changed = False
-  for dev in devices:
-    config_file = f"{ config_dir }/{ dev.name.replace(' ', '_') }.yml"
-    structured_config = open_yaml_file(config_file)
-    if structured_config == None:
-        continue
-    hash_init = hash_yaml_file(config_file)
-
-    structured_config = dev.config_context
-
-    write_yaml_file(config_file, structured_config, False)
-    hash_end = hash_yaml_file(config_file)
-
-  return has_changed if hash_init == hash_end else True
+def process_gateway(nb, struc_config, dev):
+  struct_config =  dev.config_context
 
 def main():
   module = AnsibleModule(
@@ -148,6 +118,7 @@ def main():
       token=dict(type='str', required=True),
       config_dir=dict(type='str', required=True),
       device_role=dict(type='str', required=True),
+      inventory_hostname=dict(type='str', required=True),
     ),
     supports_check_mode=True,
   )
@@ -155,18 +126,32 @@ def main():
   token = module.params['token']
   config_dir  = module.params['config_dir']
   device_role  = module.params['device_role']
+  inventory_hostname  = module.params['inventory_hostname']
   nb = pynetbox.api(nb_host,token)
   nb.http_session.verify = False
 
+  hash_init = hash_end = has_changed = False
   devices = nb.dcim.devices.filter(role=device_role)
-  if device_role == 'standalone-media-switch':
-      ret = process_switches(nb, config_dir, devices)
-  elif device_role == 'ip-to-hdmi-gateway':
-      ret = process_gateways(nb, config_dir, devices)
-  else:
-      ret = false
+  for dev in devices:
+    config_file = f"{ config_dir }/{ inventory_hostname }.yml"
+    struct_config = open_yaml_file(config_file)
+    if struct_config == None:
+        continue
+    hash_init = hash_yaml_file(config_file)
 
-  module.exit_json(changed=ret, msg=f"Received from NetBox, url= {module.params['nb_host']}")
+    if device_role == 'standalone-media-switch':
+        process_switch(nb, struct_config, dev)
+    elif device_role == 'ip-to-hdmi-gateway' or device_role == 'sdi-to-ip-gateway':
+        process_gateway(nb, struct_config, dev)
+    else:
+        continue
+
+    write_yaml_file(config_file, struct_config, False)
+    hash_end = hash_yaml_file(config_file)
+    MODULE_LOGGER.info(f"{config_file}: HASH { hash_init } -> { hash_end }")
+    has_changed = has_changed if hash_init == hash_end else True
+
+  module.exit_json(changed=has_changed, msg=f"Received from NetBox, url= {module.params['nb_host']}")
 
 if __name__ == "__main__":
   main()
